b3c25f3f2799ccd6ee9d2500ee5efd0af4078d24
moabdira <35613801+moabdira@users.noreply.github.com>
2023-07-19 22:58:55 -0700

    Update without-reverse-method.kt

--- 2108-Find-First-Palindromic-String/without-reverse-method.kt
+++ 2108-Find-First-Palindromic-String/without-reverse-method.kt
@@ -1,1 +1,26 @@
+Problem: https://leetcode.com/problems/find-first-palindromic-string-in-the-array/description/
 
+Solution:
+class Solution {
+    fun isPalindrome(word: String): Boolean {
+        var start = 0; var end = word.length - 1
+        while(start <= end){
+            if(word[start] != word[end])
+                return false
+            
+            ++start
+            --end
+        }
+
+        return true
+    }
+
+    fun firstPalindrome(words: Array<String>): String {
+        words.forEach{ 
+            if(isPalindrome(it)) 
+                return it
+        }
+
+        return ""
+    }
+}
